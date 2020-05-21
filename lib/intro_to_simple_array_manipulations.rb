fbbf4b24497e26edf389953e8ae8bd18584f5bcc
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-21 21:27:58 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array_two = array.pop
+end
+
+def pop_with _args(array)
+  
+end
+
+def using_shift
+  
+end
+
+def shift_with_args 
+  
+end
